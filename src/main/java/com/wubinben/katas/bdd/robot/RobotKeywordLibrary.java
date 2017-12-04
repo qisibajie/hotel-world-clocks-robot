6eabdb3fb237ea910276a80c213a1d14917141b8
Ben Wu <wubin28@gmail.com>
2017-12-04 15:13:13 +0800

    make all keywords green.

--- src/main/java/com/wubinben/katas/bdd/robot/RobotKeywordLibrary.java
+++ src/main/java/com/wubinben/katas/bdd/robot/RobotKeywordLibrary.java
@@ -9,9 +9,43 @@ package com.wubinben.katas.bdd.robot;
  * To change this template use File | Settings | File Templates.
  */
 public class RobotKeywordLibrary {
+    private static final BenLogger LOGGER = BenLogger.getLogger(BenLogger.LEVEL_OFF);
     /**
      *  Means that that this instance of the class well be used
      *  for whole lifecycle of test execution.
      */
     public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
+    private HotelWorldClocksFacade hotelWorldClocksFacade;
+
+    public RobotKeywordLibrary() {
+        this.hotelWorldClocksFacade = HotelWorldClocksFacade.newInstance();
+    }
+
+    public void testInitialization() {
+        this.hotelWorldClocksFacade.testInitialization();
+    }
+
+    public void setCityDstActiveAndAddToCityList(String cityName) {
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.ACTIVE);
+    }
+
+    public void setCityDstInactiveAndAddToCityList(String cityName) {
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.INACTIVE);
+    }
+
+    public void adjustIncorrectTimeOfCity(int hour, String cityName) {
+        LOGGER.info("in RobotKeywordLibrary.adjustIncorrectTimeOfCity().");
+        LOGGER.info("hour: " + hour);
+        LOGGER.info("cityName: " + cityName);
+        this.hotelWorldClocksFacade.adjustIncorrectTimeOfCity(hour, cityName);
+    }
+
+    public String getResultHour(String cityName) {
+        LOGGER.info("in RobotKeywordLibrary.getResultHour().");
+        return this.hotelWorldClocksFacade.getResultHour(cityName);
+    }
+
+    public void adjustTimeOfCityWhenDstEnds(int hour, String cityName) {
+        this.hotelWorldClocksFacade.adjustIncorrectTimeOfCity(hour, cityName);
+    }
 }
