2e48c237b89c7dceba8a972a762bf3195a6c7990
Ben Wu <wubin28@gmail.com>
2017-12-04 15:07:39 +0800

    add RobotKeywordLibrary and robotframework acceptance test cases

---
+++ src/main/java/com/wubinben/katas/bdd/robot/RobotKeywordLibrary.java
@@ -0,0 +1,17 @@
+package com.wubinben.katas.bdd.robot;
+
+
+/**
+ * The fixture code.
+ * User: Ben
+ * Date: 13-9-7
+ * Time: 下午11:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class RobotKeywordLibrary {
+    /**
+     *  Means that that this instance of the class well be used
+     *  for whole lifecycle of test execution.
+     */
+    public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
+}
